f28467ac4390fb92648095b0314568b708ca07d9
MichaelGelo <101308741+MichaelGelo@users.noreply.github.com>
2025-01-27 22:26:32 +0800

    Add project files.

---
+++ asmfunc.asm
@@ -0,0 +1,13 @@
+section .data
+
+section .text
+bits 64
+default rel
+
+global x86
+extern printf
+
+x86:
+
+	
+ret
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+extern void x86();
+
+int main() {
+	x86();
+	return 0;
+}
